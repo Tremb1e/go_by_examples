26603f06d5cb3dda04154fc35dac946045040500
Tremb1e <trembler0521@gmail.com>
2022-02-08 19:06:11 +0800

    modified:   slice.go

--- slice.go
+++ slice.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func printandLen(slice []string) {
+
+	fmt.Println(slice)
+	fmt.Println(len(slice))
+}
+
+func main() {
+
+	slice1 := make([]string, 5)
+
+	for i := 0; i < 5; i++ {
+		slice1[i] = "slice" + strconv.Itoa(i)
+	}
+	printandLen(slice1)
+	//fmt.Println(slice1)
+
+	//fmt.Println(len(slice1))
+
+	slice1 = append(slice1, "slice5", "slice6")
+	printandLen(slice1)
+	//fmt.Println(slice1)
+
+	//fmt.Println(len(slice1))
+
+	slice2 := make([]string, len(slice1))
+	copy(slice2, slice1)
+	printandLen(slice2)
+
+	fmt.Println(slice2[2:5])
+
+}
