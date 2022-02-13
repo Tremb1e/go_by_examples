d956723d60e989cbddd7ccaf823f8bce237dcb2e
Tremb1e <trembler0521@gmail.com>
2022-02-13 16:32:32 +0800

    modified:   structs.go

--- structs.go
+++ structs.go
@@ -1,1 +1,37 @@
 package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age  int
+}
+
+func newPerson(name string) *person {
+
+	p := person{name: name}
+	p.age = 18
+	return &p
+
+}
+
+func main() {
+
+	fmt.Println(person{"karl", 18})
+	fmt.Println(newPerson("kate"))
+	fmt.Println(*newPerson("kate"))
+
+	alpha := person{
+		name: "alpha",
+		age:  20,
+	}
+	fmt.Println(alpha)
+	alpha.age = 21
+	fmt.Println(alpha)
+
+	bravo := &alpha
+	bravo.age = 22
+	fmt.Println(alpha)
+	fmt.Println(bravo)
+
+}
